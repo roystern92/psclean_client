7d614ef9631775d8c3e4ccaa2f33a39d409371ca
roy <roystern92@gmail.com>
2020-02-04 13:19:09 +0200

    Changed the contact phone number

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     render() {
         return (
             <div className={classes.Footer}>
-                <h3 className={classes.Contact}>פניאל שלום | טלפון: 0502345345</h3>
+                <h3 className={classes.Contact}>פניאל שלום | טלפון: 0549887777</h3>
                 <h3 className={classes.Rights}>כל הזכויות שמורות ל: A.R.W</h3>
             </div>
         )
